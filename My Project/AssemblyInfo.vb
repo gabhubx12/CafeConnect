846e1479287c33aca25ddc116eaed03d949f050c
gabhubx12 <josegabriellorenzopineyda@gmail.com>
2026-08-23 08:19:36 -0600

    Crear My Project/AssemblyInfo.vb - Información del ensamblado

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' La información general sobre un ensamblado se controla mediante el siguiente
+' conjunto de atributos. Cambiar estos valores de atributo para modificar la información
+' asociada con un ensamblado.
+
+' Revisar los valores de los atributos del ensamblado
+
+<Assembly: AssemblyTitle("CafeConnect")>
+<Assembly: AssemblyDescription("Sistema de Gestión para Red de Cafeterías")>
+<Assembly: AssemblyCompany("CafeConnect S.A.")>
+<Assembly: AssemblyProduct("CafeConnect")>
+<Assembly: AssemblyCopyright("Copyright © 2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'El siguiente GUID es para el Id. de typelib cuando este proyecto se expone a COM
+<Assembly: Guid("a1b2c3d4-e5f6-47g8-h9i0-j1k2l3m4n5o6")>
+
+' La información de versión de un ensamblado consta de los cuatro valores siguientes:
+'
+'      Versión principal
+'      Versión secundaria
+'      Número de compilación
+'      Revisión
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
